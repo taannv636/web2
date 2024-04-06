f49e4bb8f26485e707d8a4167b83ab41ae6ff4f1
taan <130572138+taannv636@users.noreply.github.com>
2024-04-06 09:59:14 +0700

    Delete PHP1-main directory

--- PHP1-main/login/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if (isset($_COOKIE['username']) && ($_COOKIE['username'])) {
-    setcookie("username", "", time() - 30 * 24 * 60 * 60, '/');
-    setcookie("password", "", time() - 30 * 24 * 60 * 60, '/');
-    header('Location: ../index.php');
-}
