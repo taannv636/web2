ba95a99c7973a0182ca6a6f6da8dd9dc1c0ada6e
hoangphat2003 <phathcm03@gmail.com>
2024-05-11 08:06:28 +0700

    xóa inpdf ở admin

--- PHP/admin/edit.php
+++ PHP/admin/edit.php
@@ -192,7 +192,6 @@ if (isset($_GET['order_id'])) {
                     </div>
 
                     <button type="submit" class="btn btn-success" name="save">Lưu</button>
-                    <button type="button" class="btn btn-danger" id="btnOrderPDF">In PDF</button>
                
                     <?php
                     $previous = "javascript:history.go(-1)";
